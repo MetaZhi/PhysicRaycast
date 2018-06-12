b0b85540485391a27ed2d7b0fa877276b2f0bf2b
Manic <zhz@hongliu.cc>
2018-06-12 20:54:37 +0800

    初始化项目

---
+++ PhysicRaycast/Assets/Scripts/PhysicRaycast.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicRaycast : MonoBehaviour
+{
+    public LayerMask mask;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(mask.value + ": " + Convert.ToString(mask.value, 2));
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    RaycastHit hit;
+	    if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, float.MaxValue, LayerMask.GetMask("First", "Second")))
+	    {
+	        Debug.DrawRay(hit.point, hit.normal, Color.red);
+        }
+	    else if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+	    {
+	        Debug.DrawRay(hit.point, hit.normal, Color.green);
+	    }
+	}
+}
